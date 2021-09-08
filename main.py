5bf3cb242d732e3327406e3af5f51882602fcbcb
Javier Arduz Lopez <35275818+arduzj@users.noreply.github.com>
2021-09-08 17:22:47 +0000

    Second dice added

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import random 
+import random
+
 while True:     
-     print(''' 1. roll the dice             2. exit     ''')    
-     user = int(input("what you want to do\n"))     
-     if user==1:         
-        number = random.randint(1,6)         
-        print(number)     
-     else:         
-        break
+	print('''1. Roll the dice\n2. Exit''')
+	user = int(input("What you want to do?\n"))
+	if user == 1:
+		d1 = random.randint(1,6)
+		d2 = random.randint(1,6)
+		print(f'\nDice #1 = {d1}  /  Dice #2 = {d2}\n')
+		if(d1 + d2 == 7):
+			print("CONGRATS!!! You have a 7")
+	elif user == 2:
+		break
+	else:
+		print("Please read first")
